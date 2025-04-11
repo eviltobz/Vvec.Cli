acb45f46aebd4b5b660d921b8863d167efbb73e2
TobyCarterAngstrom <tobias.carter@angstromsports.com>
2025-04-11 16:38:43 +0100

    Manual git testing helper

---
+++ Vvec.Prj.Tests/GitHAXX.cs
@@ -0,0 +1,14 @@
+﻿using Vvec.Prj.Logic;
+
+namespace Vvec.Prj.Tests;
+
+public class GitHAXX
+{
+    [Test]
+    public void PokeRepo()
+    {
+        var git = new Git(@"c:/dev/eviltobz/vvec.cli");
+        var bob = git.CurrentBranch + $", {git.HasUncommittedChanges}";
+        Assert.Fail(bob);
+    }
+}
